7db92ee05a00788dcfacfa2c237125e3c7122987
Alexander Chori <alexandrchori@gmail.com>
2025-06-18 17:35:35 +0200

    Generate index-document with LLM

---
+++ packages/chaingraph-nodes/src/nodes/archai/index-document.node.ts
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2025 BadLabs
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file LICENSE.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+ */
+
+import type {
+  ArchAIContext,
+  ExecutionContext,
+  NodeExecutionResult,
+} from '@badaitech/chaingraph-types'
+import process from 'node:process'
+import { createGraphQLClient, GraphQL } from '@badaitech/badai-api'
+import {
+  BaseNode,
+  Input,
+  Node,
+  Output,
+  PortBoolean,
+  PortNumber,
+  PortString,
+} from '@badaitech/chaingraph-types'
+import { NODE_CATEGORIES } from '../../categories'
+
+@Node({
+  type: 'ArchAIIndexDocumentNode',
+  title: 'ArchAI Index Document',
+  description: 'Index a document in the ArchAI Knowledge Database',
+  category: NODE_CATEGORIES.ARCH_RAG,
+  tags: ['knowledge', 'database', 'documents', 'indexing', 'kdb'],
+})
+class ArchAIIndexDocumentNode extends BaseNode {
+  @Input()
+  @PortString({
+    title: 'Document ID',
+    description: 'ID of the document to index',
+    defaultValue: '',
+  })
+  documentId: string = ''
+
+  @Input()
+  @PortString({
+    title: 'Chat ID',
+    description: 'ID of the chat where the task is created',
+    defaultValue: '',
+  })
+  chatId: string = ''
+
+  @Input()
+  @PortString({
+    title: 'Message ID',
+    description: 'ID of the message that triggered the task',
+    defaultValue: '0',
+  })
+  messageId: string = '0'
+
+  @Input()
+  @PortNumber({
+    title: 'Chunk Size Tokens',
+    description: 'Size of chunks in tokens',
+    defaultValue: 500,
+  })
+  chunkSizeTokens: number = 500
+
+  @Input()
+  @PortNumber({
+    title: 'Chunk Overlap Tokens',
+    description: 'Number of tokens to overlap between chunks',
+    defaultValue: 150,
+  })
+  chunkOverlapTokens: number = 150
+
+  @Input()
+  @PortNumber({
+    title: 'Cost Limit',
+    description: 'Maximum cost allowed for indexing',
+    defaultValue: 0.0,
+  })
+  costLimit: number = 0.0
+
+  @Input()
+  @PortBoolean({
+    title: 'Need QA',
+    description: 'Whether QA pairs should be generated',
+    defaultValue: false,
+  })
+  needQA: boolean = false
+
+  @Input()
+  @PortNumber({
+    title: 'QA Count Per Run',
+    description: 'Number of QA pairs to generate per run',
+    defaultValue: 15,
+  })
+  qaCountPerRun: number = 15
+
+  @Input()
+  @PortString({
+    title: 'QA Model',
+    description: 'Model to use for generating QA pairs',
+    defaultValue: 'gpt-4o-mini',
+  })
+  qaModel: string = 'gpt-4o-mini'
+
+  @Input()
+  @PortString({
+    title: 'Instruction for QA',
+    description: 'Instructions for generating QA pairs',
+    defaultValue: '',
+  })
+  instructionForQA: string = ''
+
+  @Input()
+  @PortBoolean({
+    title: 'Force Confirm',
+    description: 'Whether to force confirmation',
+    defaultValue: false,
+  })
+  forceConfirm: boolean = false
+
+  @Output()
+  @PortString({
+    title: 'Task ID',
+    description: 'ID of the created indexing task',
+    defaultValue: '',
+  })
+  taskId: string = ''
+
+  async execute(context: ExecutionContext): Promise<NodeExecutionResult> {
+    // Validate required inputs
+    if (!this.documentId?.trim()) {
+      throw new Error('Document ID is required')
+    }
+
+    const archAIContext = context.getIntegration<ArchAIContext>('archai')
+    const agentSession = archAIContext?.agentSession
+    if (!agentSession) {
+      throw new Error('ArchAI agent session is not available in the context')
+    }
+
+    const graphQLClient = createGraphQLClient(
+      process.env.BADAI_API_URL || 'http://localhost:9151/graphql',
+    )
+
+    // Prepare an indexing task input
+    const indexingInput = {
+      document_id: this.documentId,
+      chat_id: this.chatId,
+      message_id: this.messageId,
+      chunk_size_tokens: this.chunkSizeTokens,
+      chunk_overlap_tokens: this.chunkOverlapTokens,
+      cost_limit: this.costLimit,
+      need_qa: this.needQA,
+      qa_count_per_run: this.qaCountPerRun,
+      qa_model: this.qaModel,
+      instruction_for_qa: this.instructionForQA,
+      force_confirm: this.forceConfirm,
+    }
+
+    // Create an indexing task in the knowledge database
+    const { kdbIndexingDocumentTaskCreate } = await graphQLClient.request(
+      GraphQL.KdbIndexingDocumentTaskCreateDocument,
+      {
+        session: agentSession,
+        input: indexingInput,
+      },
+    )
+
+    if (!kdbIndexingDocumentTaskCreate) {
+      throw new Error('Failed to create indexing task')
+    }
+
+    // Set the task ID output
+    this.taskId = kdbIndexingDocumentTaskCreate.task_id
+
+    return {}
+  }
+}
+
+export default ArchAIIndexDocumentNode
